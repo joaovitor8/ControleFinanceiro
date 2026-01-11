8bdeae557223dbedd6169d48b9d2d0a5b3aac0b4
João Vitor <joaoezeki@gmail.com>
2026-01-11 13:12:02 -0300

    Iniciando Projeto

--- web/app/layout.tsx
+++ web/app/layout.tsx
@@ -20,7 +20,7 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`} >
         {children}
       </body>
     </html>
